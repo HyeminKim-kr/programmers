730372e866eb14f8ea63cb3ef4f1c6e1da4e3731
Hyemin Kim <58822591+HyeminKim-kr@users.noreply.github.com>
2024-08-06 16:33:46 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,12 +1,10 @@
 def solution(price):
     if price >= 500000:
-        discounted_price = price * 0.8
+        return int(price*0.8)
     elif price >= 300000:
-        discounted_price = price * 0.9
+        return int(price*0.9)
     elif price >= 100000:
-        discounted_price = price * 0.95
+        return int(price*0.95)
     else:
-        discounted_price = price
+        return price
     
-    answer = int(discounted_price)
-    return answer
