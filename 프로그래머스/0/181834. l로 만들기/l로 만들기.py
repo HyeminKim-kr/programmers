8eb27e33232763b813cda076c20d880e3851ec1d
Hyemin Kim <58822591+HyeminKim-kr@users.noreply.github.com>
2024-09-16 17:03:49 +0900

    [level 0] Title: l로 만들기, Time: 89.30 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,3 +1,7 @@
 def solution(myString):
-    answer = ''
-    return answer
+    til_l = 'abcdefghijk'
+    for s in myString:
+        if s in til_l:
+            myString = myString.replace(s, 'l')
+    return myString
+            
